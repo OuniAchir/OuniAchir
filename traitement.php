4e2b8d9d220371494df10c953decb3678f88abbb
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2024-02-15 22:23:37 +0100

    Update traitement.php
    J'ai ajouté les tokens 
    J'ai filtré les données saisies
    J'ai remplacé le code de JS en Php

--- traitement.php
+++ traitement.php
@@ -1,8 +1,14 @@
 <?php
-$servername= "localhost";
-$username="root";
-$password="";
-$dbname= "devoir_securite";
+session_start();
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+$token = $_SESSION['token'];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "devoir_securite";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -10,52 +16,49 @@ try {
 } catch(PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
+if (isset($_POST['token']) && hash_equals($_SESSION['token'], $_POST['token'])) {
+    if (isset($_POST['ok'])) {
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
+        $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+        $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
+        $password = $_POST['password']; 
+        $confirme = $_POST['confirme'];
 
-if (isset($_POST['ok'])) {
-    extract($_POST);
-    if ($password !== $confirme) {
-        echo "<script>alert('Les mots de passe ne correspondent pas. Veuillez réessayer.');</script>";
-    } else {
-    // Vérifier si l'e-mail existe déjà
-    $checkEmailQuery = $conn->prepare("SELECT COUNT(*) AS count FROM users WHERE email = :email");
-    $checkEmailQuery->execute([':email' => $email]);
-    $emailExists = $checkEmailQuery->fetch(PDO::FETCH_ASSOC)['count'];
-
-    // Vérifier si le pseudo existe déjà
-    $checkPseudoQuery = $conn->prepare("SELECT COUNT(*) AS count FROM users WHERE pseudo = :pseudo");
-    $checkPseudoQuery->execute([':pseudo' => $pseudo]);
-    $pseudoExists = $checkPseudoQuery->fetch(PDO::FETCH_ASSOC)['count'];
-
-        if ($emailExists > 0) {
-        //echo "L'e-mail existe déjà. Veuillez en choisir un autre.";
-            echo "<script>alert('L'e-mail existe déjà. Veuillez en choisir un autre.');</script>";
-        } elseif ($pseudoExists > 0) {
-        //echo "Le pseudo existe déjà. Veuillez en choisir un autre.";
-            echo "<script>alert('Le pseudo existe déjà. Veuillez en choisir un autre.');</script>";
+        if ($password !== $confirme) {
+            echo htmlentities('Les mots de passe ne correspondent pas. Veuillez réessayer.');
         } else {
-        // Si ni l'e-mail ni le pseudo n'existent
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $checkEmailQuery = $conn->prepare("SELECT COUNT(*) AS count FROM users WHERE email = :email");
+            $checkEmailQuery->execute([':email' => $email]);
+            $emailExists = $checkEmailQuery->fetch(PDO::FETCH_ASSOC)['count'];
 
-            $insertQuery = $conn->prepare("INSERT INTO users VALUES (0, :pseudo, :nom, :prenom, :email, :password)");
-            $insertQuery->execute(
-                array(
+            $checkPseudoQuery = $conn->prepare("SELECT COUNT(*) AS count FROM users WHERE pseudo = :pseudo");
+            $checkPseudoQuery->execute([':pseudo' => $pseudo]);
+            $pseudoExists = $checkPseudoQuery->fetch(PDO::FETCH_ASSOC)['count'];
+
+            if ($emailExists > 0) {
+                echo htmlentities('E-mail existe déjà. Veuillez en choisir un autre.');
+            } elseif ($pseudoExists > 0) {
+                echo htmlentities('Le pseudo existe déjà. Veuillez en choisir un autre.');
+            } else {
+                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+                $insertQuery = $conn->prepare("INSERT INTO users (pseudo, nom, prenom, email, password) VALUES (:pseudo, :nom, :prenom, :email, :password)");
+                $insertQuery->execute([
                     ':pseudo' => $pseudo,
                     ':nom' => $nom,
                     ':prenom' => $prenom,
                     ':email' => $email,
                     ':password' => $hashedPassword
-                )
-            );
-            //$reponse =$requete->fetch(PDO::FETCH_ASSOC);
-            //var_dump($reponse);
-            //echo 'Inscription réussie, bienvenue!';
-            //echo "<script>alert('Inscription réussie, bienvenue!');</script>";
-            header("Location: connexion.php");
-            exit();
+                ]);
+                header("Location: connexion.php");
+                exit();
+            }
         }
-    } 
-} elseif (isset($_POST['reset'])) {
-    $pseudo = $nom = $prenom = $email = $password = '';
-    //echo 'Formulaire réinitialisé!';
+    } elseif (isset($_POST['reset'])) {
+                $pseudo = $nom = $prenom = $email = $password = '';
+                //echo 'Formulaire réinitialisé!';
+    }
+}else {
+    die("Erreur de sécurité CSRF.");
 }
 ?>
